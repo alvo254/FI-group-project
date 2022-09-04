6b065ec6d6f25f698f88b2a1c6a7c49cf0fcf14e
alvo254 <alvinkhamron@gmail.com>
2022-09-04 15:06:11 +0300

    editing and update

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import './Reviews.css'
+import {FaTrash, FaEdit} from "react-icons/fa"
 // import {uuid} from "uuidv4"
 
 const Reviews = () => {
 
   const [form, setForm] = useState({resturant: "", review:"", id:null })
   const [reviews, setReviews] = useState([])
+  const [edit, setEditing] = useState(false)
 
   const sendit = (e) => {
     e.preventDefault()
@@ -14,14 +16,35 @@ const Reviews = () => {
 
   }
 
+  const update = e =>{
+    e.preventDefault()
+    setEditing(false)
+    const updatedRevs = reviews.map(review => review.id == form.id ? form : review)
+
+  }
+  
+  const handleEdit = id => {
+    const editPost = reviews.filter(review => review.id == id)
+    setForm(editPost[0])
+    setEditing(true)
+  }
+
   const change = e =>{
     const {name, value} = e.target
     setForm({...form, [name]:value})
   }
 
+  const deleting = id => {
+    const uprev = reviews.filter(review => 
+      review.id != id
+      )
+      setReviews(uprev)
+    
+  }
+
   return (
     <div className='master'>
-      <form action="" onSubmit={sendit} className="form">
+      <form action="" onSubmit={edit ? update : sendit } className="form">
         <h2>leave a comment</h2>
         <label> Resturant</label>
         <input type="text" 
@@ -32,13 +55,17 @@ const Reviews = () => {
         <textarea type="text" 
         placeholder='Please leave a review' 
         id="review" name='review' value={form.review} onChange={change}/>
-        <button type='submit'>submit</button>
+        <button type='submit'>{edit ? "Update" : "Submit"}</button>
       </form>
       <div className='revlist'>
         {reviews.map((review) => 
         <div className='revitem'>
           <h2>{review.resturant}</h2>
           <p>{review.review}</p>
+          <div className="buttons">
+            <button onClick={() => deleting(review.id)}> <FaTrash/> </button>
+            <button onClick={() => handleEdit(review.id)}><FaEdit/></button>
+          </div>
         </div>
         )}
 
