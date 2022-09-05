bed278d6c17c0a1790985bebbcca0e47fb4732ad
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-05 21:38:46 +0300

    [add] home page [change] navbar height & [style] home page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const NavBar =() => {
         <div className="navbar">
             <span className="nav-logo">Restaurant Review</span>
             <div className="nav-items">
-                <a href="/home">Home</a>
+                <a href="/">Home</a>
                 <a href="/about">About</a>
                 <a href="/contact">Contact Us</a>
                 <a href="/reviews">Add Resturant</a>
