86584b84ba8945491bba5334be1b732ae64e8ed3
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-05 20:18:35 +0300

    [fetch] reviews  and style reviews

--- src/components/Restuarants/Restuarants.jsx
+++ src/components/Restuarants/Restuarants.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
-import logo from "../../assets/logo.JPG"
+import Comment from '../Comment/Comment'
+import {useEffect, useState} from "react";
+
 
 const Restuarants = () => {
+  const [restaurants, setRestaurant] = useState([])
+
+    useEffect(()=>{
+        fetch("https://rack-hosting-1.herokuapp.com/reviews")
+        .then(res=>res.json())
+        .then((data) =>
+        setRestaurant(data)   
+        )
+    },[])
+    console.log(restaurants)
+
+    const allComments = restaurants.map((restaurant)=>{
+      return( <Comment  key={restaurant.id}
+        name={restaurant.name}
+        location ={restaurant.location}
+        body = {restaurant.reviews.map(review => {
+            return review.body     
+        })}
+         user = {restaurant.reviews.map(review => {
+            return review.user.name
+
+         })}
+
+        />)
+    })
+    return(
+    <>
+    <div className="allCommnets">
+    {allComments}
+    </div>
+    </>
+    )
   
 
 
 
-  return (
-    <main>
-    <section className="menu section">
-     
-    </section>
-  </main>
-  )
+  
 }
 
 export default Restuarants
