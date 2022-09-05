0f28a3142697057a2b256cf93a5c0d274f1f4a0b
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-05 21:56:37 +0300

    [add] about us and styled

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import "./About.css"
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h1>About</h1>
+    <div className="about">
+    <div className="img">
+    <img src={process.env.PUBLIC_URL + '/images/img3.jpg'} alt="Valentine" className='image1'></img>
+    </div>
+        <div className="content">
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
+        </div>
+    </div>
+    <div className="about">
+        <div className="content">
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
+        </div>
+        <div className="img">
+        <img src={process.env.PUBLIC_URL + '/images/istockphoto-694189032-170667a.jpg'} alt="Valentine" className='image1'></img>
+        </div>
+    </div>
+    </>
+    
   )
 }
 
